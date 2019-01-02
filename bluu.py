c9a2e32e2a1b85532a64b5c742253c20233c7e37
Eiscookie <46327749+Eiscookie@users.noreply.github.com>
2019-01-02 19:45:04 +0100

    Add files via upload

---
+++ bluu.py
@@ -0,0 +1,18 @@
+import turtle
+
+bluu = turtle.Turtle()
+
+i = 100
+while i > 2:
+   bluu.forward(20)
+   bluu.left(20)
+   bluu.forward(60)
+   bluu.left(10)
+   bluu.backward(30)
+   bluu.left(40)
+   bluu.forward(10)
+   bluu.backward(40)
+
+
+   
+turtle.done()
